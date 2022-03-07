b171c845282e32afc36f98a31fb14736107cd6bc
Tuncay Dagdelen <sramos083@gmail.com>
2022-03-07 14:30:07 +0100

    i changed

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function renderPokemon() {
     document.getElementById('main').innerHTML = ``;
     for (let i = 0; i < searchedPokemons.length; i++) {
         const pokemon = searchedPokemons[i];
-        renderPokemonCard(pokemon);
-        renderPokemonInfo(pokemon);
-        types(pokemon);
-        about(pokemon);
+        renderPokemonCard(pokemon, i);
+        renderPokemonInfo(pokemon, i);
+        types(pokemon, i);
+        about(pokemon, i);
         // baseStats(pokemon);
         // moves(pokemon);
     }
@@ -31,40 +31,40 @@ function searchPokemon() {
 }
 
 
-function renderPokemonInfo(pokemon) {
-    document.getElementById('pokemonName' + pokemon['id']).innerHTML = pokemon['name'];
-    document.getElementById('pokemonImage' + pokemon['id']).src = pokemon['sprites']['other']['dream_world']['front_default'];
-    document.getElementById('type' + pokemon['id']).innerHTML = pokemon['types'][0]['type']['name'];
+function renderPokemonInfo(pokemon, i) {
+    document.getElementById('pokemonName' + pokemon[i]).innerHTML = pokemon['name'];
+    document.getElementById('pokemonImage' + pokemon[i]).src = pokemon['sprites']['other']['dream_world']['front_default'];
+    document.getElementById('type' + pokemon[i]).innerHTML = pokemon['types'][0]['type']['name'];
     // console.log(searchedPokemons)
 }
 
 
-function about(pokemon) {
-    aboutHTML(pokemon);
-    document.getElementById('species' + pokemon['id']).innerHTML = pokemon['types'][0]['type']['name'];
-    document.getElementById('height' + pokemon['id']).innerHTML = pokemon['height'] + '0 cm';
-    document.getElementById('weight' + pokemon['id']).innerHTML = pokemon['weight'] + ' kg';
+function about(pokemon, i) {
+    aboutHTML(pokemon, i);
+    document.getElementById('species' + pokemon[i]).innerHTML = pokemon['types'][0]['type']['name'];
+    document.getElementById('height' + pokemon[i]).innerHTML = pokemon['height'] + '0 cm';
+    document.getElementById('weight' + pokemon[i]).innerHTML = pokemon['weight'] + ' kg';
     for (let i = 0; i < pokemon['abilities'].length; i++) {
         const element = pokemon['abilities'][i];
         let ability = element['ability']['name'];
-        document.getElementById('abilities' + pokemon['id']).innerHTML += `<div class="abilities-child">${ability},</div>`;
+        document.getElementById('abilities' + pokemon[i]).innerHTML += `<div class="abilities-child">${ability},</div>`;
     }
 }
 
-function baseStats(pokemon) {
-    baseStatsHTML(pokemon);
-    document.getElementById('hp' + pokemon['id']).innerHTML = pokemon['stats'][0]['base_stat'];
-    document.getElementById('attack' + pokemon['id']).innerHTML = pokemon['stats'][1]['base_stat'];
-    document.getElementById('defense' + pokemon['id']).innerHTML = pokemon['stats'][2]['base_stat'];
-    document.getElementById('special-attack' + pokemon['id']).innerHTML = pokemon['stats'][3]['base_stat'];
+function baseStats(pokemon, i) {
+    baseStatsHTML(pokemon, i);
+    document.getElementById('hp' + pokemon[i]).innerHTML = pokemon['stats'][0]['base_stat'];
+    document.getElementById('attack' + pokemon[i]).innerHTML = pokemon['stats'][1]['base_stat'];
+    document.getElementById('defense' + pokemon[i]).innerHTML = pokemon['stats'][2]['base_stat'];
+    document.getElementById('special-attack' + pokemon[i]).innerHTML = pokemon['stats'][3]['base_stat'];
 }
 
 
-function moves(pokemon) {
-    movesHTML(pokemon);
+function moves(pokemon, i) {
+    movesHTML(pokemon, i);
     for (let i = 0; i < 4; i++) {
         const element = pokemon['moves'][i];
         let move = element['move']['name'];
-        document.getElementById('move' + pokemon['id']).innerHTML += move + ', ';
+        document.getElementById('move' + pokemon[i]).innerHTML += move + ', ';
     }
 }
--- templates.js
+++ templates.js
@@ -1,78 +1,78 @@
-function renderPokemonCard(pokemon){
+function renderPokemonCard(pokemon, i){
     document.getElementById('main').innerHTML += `
     <div class="pokemonCard">
-    <div class="pokedex" id="pokedex${pokemon['id']}">
-        <h1 id="pokemonName${pokemon['id']}">Name</h1>
-        <span class="type" id="type${pokemon['id']}">Type</span>
-        <img id="pokemonImage${pokemon['id']}">
+    <div class="pokedex" id="pokedex${pokemon[i]}">
+        <h1 id="pokemonName${pokemon[i]}">Name</h1>
+        <span class="type" id="type${pokemon[i]}">Type</span>
+        <img id="pokemonImage${pokemon[i]}">
     </div>
 
     <div class="info-container">
         <div class="tabs">
-            <span id="about${pokemon['id']}" onclick="about(${pokemon['id']})" class="tab">About</span>
-            <span id="base-stats${pokemon['id']}"  onclick="baseStats(${pokemon['id']})" class="tab">Base Stats</span>
-            <span id="moves${pokemon['id']}"  onclick="moves(${pokemon['id']})" class="tab">Moves</span>
+            <span id="about${pokemon[i]}" onclick="about(${pokemon[i]})" class="tab">About</span>
+            <span id="base-stats${pokemon[i]}"  onclick="baseStats(${pokemon[i]})" class="tab">Base Stats</span>
+            <span id="moves${pokemon[i]}"  onclick="moves(${pokemon[i]})" class="tab">Moves</span>
         </div>
 
-        <div id="table${pokemon['id']}"></div>
+        <div id="table${pokemon[i]}"></div>
 
 
     </div>
     </div>`;
 
 }
-function aboutHTML(pokemon){
-    document.getElementById('table' + pokemon['id']).innerHTML =`
+function aboutHTML(pokemon, i){
+    document.getElementById('table' + pokemon[i]).innerHTML =`
     <table class="table">
     <tr>
         <td><b>Species:</b></td>
-        <td id="species${pokemon['id']}"></td>
+        <td id="species${pokemon[i]}"></td>
     </tr>
     <tr>
         <td><b>Height:</b></td>
-        <td id="height${pokemon['id']}"></td>
+        <td id="height${pokemon[i]}"></td>
     </tr>
     <tr>
         <td><b>Weight:</b></td>
-        <td id="weight${pokemon['id']}"></td>
+        <td id="weight${pokemon[i]}"></td>
     </tr>
     <tr>
         <td><b>Abilities:</b></td>
-        <td id="abilities${pokemon['id']}"></td>
+        <td id="abilities${pokemon[i]}"></td>
     </tr>
     </table>
     `;
 }
 
-function baseStatsHTML(pokemon){
-    document.getElementById('table' + pokemon['id']).innerHTML = `
+function baseStatsHTML(pokemon, i){
+    document.getElementById('table' + pokemon[i]).innerHTML = `
     <table class="table">
     <tr>
         <td><b>HP:</b></td>
-        <td id="hp${pokemon['id']}"></td>
+        <td id="hp${pokemon[i]}"></td>
     </tr>
     <tr>
         <td><b>Attack:</b></td>
-        <td id="attack${pokemon['id']}"></td>
+        <td id="attack${pokemon[i]}"></td>
     </tr>
     <tr>
         <td><b>Defense:</b></td>
-        <td id="defense${pokemon['id']}"></td>
+        <td id="defense${pokemon[i]}"></td>
     </tr>
     <tr>
         <td><b>Special Attack:</b></td>
-        <td id="special-attack${pokemon['id']}"></td>
+        <td id="special-attack${pokemon[i]}"></td>
     </tr>
     </table>
     `;
 }
 
-function movesHTML(pokemon){
-    document.getElementById('table' + pokemon['id']).innerHTML = `
+function movesHTML(pokemon, i){
+    document.getElementById('table' + pokemon[i]).innerHTML = `
     <table class="table">
     <tr>
         <td><b>Moves:</b></td>
-        <td id="move${pokemon['id']}"></td>
+        <td id="move${pokemon[i]}"></td>
     </tr>
     </table>
     `;
